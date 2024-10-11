6d63982137f878e51cbefe54fc87a285eb4f70c3
Justin Wu <justin_wu@ucsb.edu>
2024-10-10 23:18:21 -0700

    rewrote solver, updated files

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
 # lemondrop
 
-A solver for a funny little game featured in the 1993 Wizard of Oz video game for the SNES. The game can also be played in the web [here](https://aphotic.space/pub/games/lmndrop/). The rules are simple: you start with the numbers 1-12 available, and every turn, you roll two dice. Take the sum of the two dice, and to finish your turn, you look at your numbers and either choose to use up the sum or two other numbers that add up to the sum of the dice. For example, if you roll a 3, you can either use up your 3 or use up both the 1 and 2. You can only use each number from 1-12 once. As you might discover, the game is far from easy to win - for starters, you'll have to roll two sixes (a 1/36 chance!) at some point to use up your 12.
+A solver for a funny little game called Lemon Drop Elimination featured in the 1993 Wizard of Oz video game for the SNES. The game can also be played in the web [here](https://aphotic.space/pub/games/lmndrop/). The rules are simple: you start with the numbers 1-12 available, and every turn, you roll two dice. Take the sum of the two dice, and to finish your turn, you look at your numbers and either choose to use up the sum or two other numbers that add up to the sum of the dice. For example, if you roll a 3, you can either use up your 3 or use up both the 1 and 2. You can only use each number from 1-12 once. As you might discover, the game is far from easy to win - for starters, you'll have to roll two sixes (a 1/36 chance!) at some point to use up your 12.
 
 ## Solver
 
-The solver is quite rough around the edges, wrote it quite long ago. Uses a dynamic programming approach to determine the expected value of simpler game states and works its way up to the starting state. The game states are represented with a 12-bit number: the leftmost bit represents whether the 1 is available (1 for available, 0 for unavailable), 2nd leftmost represents the 2, and so on.
+The solver uses a dynamic programming approach to determine the expected value of simpler game states and works its way up to the starting state. The game states are represented with a 12-bit number: the leftmost bit represents whether the 1 is available (1 for available, 0 for unavailable), 2nd leftmost represents the 2, and so on.
 
 ## Files
-
-The `lemondropodds.py` file calculates the expected values of all game states and produces a text file `dropsodds.txt` that displays the results.
 
 The `lemondropsolver.py` file calculates the expected values of all game states and stores all optimal decisions. The file `dropsolver.txt` is produced which displays the results in a table. The table is read by first finding your game state - for instance, 101101010111 indicates that you have 1, 3, 4, 6, 8, 10, 11, 12 still available and 2, 5, 7, 9 unavailable. Then, find the column for the dice roll that you receive to find the optimal decision. A 0 means that you have no possible moves, and have lost. The expected values are also shown in the file - as you can see, you start with an expected value of 0.00286 which means you have a 0.286% (1 in 350) chance of rolling well enough to have a winnable game.
--- lemondropsolver.py
+++ lemondropsolver.py
@@ -1,20 +1,8 @@
-import numpy as np
 from prettytable import PrettyTable
 
-# dice roll | prob
-# 1         | 0
-# 2         | 0.0277
-# 3         | 0.0555
-# 4         | 0.0833
-# 5         | 0.1111
-# 6         | 0.1388
-# 7         | 0.1666
-# 8         | 0.1388
-# 9         | 0.1111
-# 10        | 0.0833
-# 11        | 0.0555
-# 12        | 0.0277
+target_name = 'dropsolver.txt' # name of results file that will be written to
 
+# dictionary of probabilities of rolling each sum of two dice. will be used as weights to calculate expected values of each state
 prob_dict = {2: 1/36, 
              3: 2/36, 
              4: 3/36, 
@@ -29,333 +17,81 @@ prob_dict = {2: 1/36,
 
 x_value_dict = {0: 1} # contains expected values for every game state. start with won game state w/ expected value 1
 
-od_2 = [0] # optimal decisions for rolling 2
-od_3 = [0] # optimal decisions for rolling 3
-od_4 = [0] # optimal decisions for rolling 4
-od_5 = [0] # optimal decisions for rolling 5
-od_6 = [0] # optimal decisions for rolling 6
-od_7 = [0] # optimal decisions for rolling 7
-od_8 = [0] # optimal decisions for rolling 8
-od_9 = [0] # optimal decisions for rolling 9
-od_10 = [0] # optimal decisions for rolling 10
-od_11 = [0] # optimal decisions for rolling 11
-od_12 = [0] # optimal decisions for rolling 12
+# a 2d list of all optimal decisions for each roll. optimal_decisions[roll] = list of optimal decisions for that roll
+# optimal_decisions[roll][state] = optimal decision for a specific state and roll
+optimal_decisions = [] 
+
+for i in range(0, 13): # first two lists will stay empty as you cannot roll 0 or 1. next 11 lists will contain optimal decisions for rolls 2-12
+    optimal_decisions.append([[0]]) # add [0] move at pos 0 to all lists as the finished game state (state = 0) is won
+
+def get_valid_moves(roll): # returns a list of all possible moves for a given roll. each element is a valid move, represented as a list of either 1 or 2 ints
+    moves = [[0], [roll]] # [0] move exists in case there are no other valid moves - if it's the only move, you lose
+    i = 1
+    while i < roll / 2:
+        j = roll - i
+        moves.append([i, j])
+        i += 1
+    return moves
 
 def int_to_bin(int):
     return f'{int:012b}'
 
-def get_x_values(state): # returns the possible x_values for each roll. Index 2 = xValue if a 2 is rolled with the current state, etc.
-    drop_state = int_to_bin(state)
-    x_values = [0, 0]
-    
-    # if you roll 2
-    if drop_state[1] == '1': # 2 available
-        x_values.append(get_x_value(state - 1024))
-        od_2.append('2')
-    else: # you lose
-        x_values.append(0)
-        od_2.append('0')
-    
-    # if you roll 3
-    move_names_3 = ['0', '1 2', '3']
-    possible_moves_3 = [0]
-    if drop_state[0] == '1' and drop_state[1] == '1': # 1 and 2 available
-        possible_moves_3.append(get_x_value(state - 3072))
-    else:
-        possible_moves_3.append(0)
-
-    if drop_state[2] == '1': # 3 available
-        possible_moves_3.append(get_x_value(state - 512))
-    else:
-        possible_moves_3.append(0)
-
-    x_values.append(np.max(possible_moves_3))
-    ind3 = possible_moves_3.index(np.max(possible_moves_3))
-    od_3.append(move_names_3[ind3])
-
-    # if you roll 4
-    move_names_4 = ['0', '1 3', '4']
-    possible_moves_4 = [0]
-    if drop_state[0] == '1' and drop_state[2] == '1': # 1 and 3 available
-        possible_moves_4.append(get_x_value(state - 2560))
-    else:
-        possible_moves_4.append(0)
-        
-    if drop_state[3] == '1': # 4 available
-        possible_moves_4.append(get_x_value(state - 256))
-    else:
-        possible_moves_4.append(0)
-        
-    x_values.append(np.max(possible_moves_4))
-    ind4 = possible_moves_4.index(np.max(possible_moves_4))
-    od_4.append(move_names_4[ind4])
-
-    # if you roll 5
-    move_names_5 = ['0', '1 4', '2 3', '5']
-    possible_moves_5 = [0]
-    if drop_state[0] == '1' and drop_state[3] == '1': # 1 and 4 available
-        possible_moves_5.append(get_x_value(state - 2304))
-    else:
-        possible_moves_5.append(0)
-
-    if drop_state[1] == '1' and drop_state[2] == '1': # 2 and 3 available
-        possible_moves_5.append(get_x_value(state - 1536))
-    else:
-        possible_moves_5.append(0)
-
-    if drop_state[4] == '1': # 5 available
-        possible_moves_5.append(get_x_value(state - 128))
-    else:
-        possible_moves_5.append(0)
-
-    x_values.append(np.max(possible_moves_5))
-    ind5 = possible_moves_5.index(np.max(possible_moves_5))
-    od_5.append(move_names_5[ind5])
-
-    # if you roll 6
-    move_names_6 = ['0', '1 5', '2 4', '6']
-    possible_moves_6 = [0]
-    if drop_state[0] == '1' and drop_state[4] == '1': # 1 and 5 available
-        possible_moves_6.append(get_x_value(state - 2176))
-    else:
-        possible_moves_6.append(0)
-
-    if drop_state[1] == '1' and drop_state[3] == '1': # 2 and 4 available
-        possible_moves_6.append(get_x_value(state - 1280))
-    else:
-        possible_moves_6.append(0)
-
-    if drop_state[5] == '1': # 6 available
-        possible_moves_6.append(get_x_value(state - 64))
-    else:
-        possible_moves_6.append(0)
-
-    x_values.append(np.max(possible_moves_6))
-    ind6 = possible_moves_6.index(np.max(possible_moves_6))
-    od_6.append(move_names_6[ind6])
-
-    # if you roll 7
-    move_names_7 = ['0', '1 6', '2 5', '3 4', '7']
-    possible_moves_7 = [0]
-    if drop_state[0] == '1' and drop_state[5] == '1': # 1 and 6 available
-        possible_moves_7.append(get_x_value(state - 2112))
-    else:
-        possible_moves_7.append(0)
-
-    if drop_state[1] == '1' and drop_state[4] == '1': # 2 and 5 available
-        possible_moves_7.append(get_x_value(state - 1152))
-    else:
-        possible_moves_7.append(0)
-
-    if drop_state[2] == '1' and drop_state[3] == '1': # 3 and 4 available
-        possible_moves_7.append(get_x_value(state - 768))
-    else:
-        possible_moves_7.append(0)
-    
-    if drop_state[6] == '1': # 7 available
-        possible_moves_7.append(get_x_value(state - 32))
-    else:
-        possible_moves_7.append(0)
-
-    x_values.append(np.max(possible_moves_7))
-    ind7 = possible_moves_7.index(np.max(possible_moves_7))
-    od_7.append(move_names_7[ind7])
-
-    # if you roll 8
-    move_names_8 = ['0', '1 7', '2 6', '3 5', '8']
-    possible_moves_8 = [0]
-    if drop_state[0] == '1' and drop_state[6] == '1': # 1 and 7 available
-        possible_moves_8.append(get_x_value(state - 2080))
-    else:
-        possible_moves_8.append(0)
-    
-    if drop_state[1] == '1' and drop_state[5] == '1': # 2 and 6 available
-        possible_moves_8.append(get_x_value(state - 1088))
-    else:
-        possible_moves_8.append(0)
-
-    if drop_state[2] == '1' and drop_state[4] == '1': # 3 and 5 available
-        possible_moves_8.append(get_x_value(state - 640))
-    else:
-        possible_moves_8.append(0)
-
-    if drop_state[7] == '1': # 8 available
-        possible_moves_8.append(get_x_value(state - 16))
-    else:
-        possible_moves_8.append(0)
-
-    x_values.append(np.max(possible_moves_8))
-    ind8 = possible_moves_8.index(np.max(possible_moves_8))
-    od_8.append(move_names_8[ind8])
-
-    # if you roll 9
-    move_names_9 = ['0', '1 8', '2 7', '3 6', '4 5', '9']
-    possible_moves_9 = [0]
-    if drop_state[0] == '1' and drop_state[7] == '1': # 1 and 8 available
-        possible_moves_9.append(get_x_value(state - 2064))
-    else:
-        possible_moves_9.append(0)
-
-    if drop_state[1] == '1' and drop_state[6] == '1': # 2 and 7 available
-        possible_moves_9.append(get_x_value(state - 1056))
-    else:
-        possible_moves_9.append(0)
-
-    if drop_state[2] == '1' and drop_state[5] == '1': # 3 and 6 available
-        possible_moves_9.append(get_x_value(state - 576))
-    else:
-        possible_moves_9.append(0)
-
-    if drop_state[3] == '1' and drop_state[4] == '1': # 4 and 5 available
-        possible_moves_9.append(get_x_value(state - 384))
-    else:
-        possible_moves_9.append(0)
-
-    if drop_state[8] == '1': # 9 available
-        possible_moves_9.append(get_x_value(state - 8))
-    else:
-        possible_moves_9.append(0)
-
-    x_values.append(np.max(possible_moves_9))
-    ind9 = possible_moves_9.index(np.max(possible_moves_9))
-    od_9.append(move_names_9[ind9])
-
-    # if you roll 10
-    move_names_10 = ['0', '1 9', '2 8', '3 7', '4 6', '10']
-    possible_moves_10 = [0]
-    if drop_state[0] == '1' and drop_state[8] == '1': # 1 and 9 available
-        possible_moves_10.append(get_x_value(state - 2056))
-    else:
-        possible_moves_10.append(0)
-
-    if drop_state[1] == '1' and drop_state[7] == '1': # 2 and 8 available
-        possible_moves_10.append(get_x_value(state - 1040))
-    else:
-        possible_moves_10.append(0)
-
-    if drop_state[2] == '1' and drop_state[6] == '1': # 3 and 7 available
-        possible_moves_10.append(get_x_value(state - 544))
-    else:
-        possible_moves_10.append(0)
-
-    if drop_state[3] == '1' and drop_state[5] == '1': # 4 and 6 available
-        possible_moves_10.append(get_x_value(state - 320))
-    else:
-        possible_moves_10.append(0)
-
-    if drop_state[9] == '1': # 10 available
-        possible_moves_10.append(get_x_value(state - 4))
-    else:
-        possible_moves_10.append(0)
-
-    x_values.append(np.max(possible_moves_10))
-    ind10 = possible_moves_10.index(np.max(possible_moves_10))
-    od_10.append(move_names_10[ind10])
-
-    # if you roll 11
-    move_names_11 = ['0', '1 10', '2 9', '3 8', '4 7', '5 6', '11']
-    possible_moves_11 = [0]
-    if drop_state[0] == '1' and drop_state[9] == '1': # 1 and 10 available
-        possible_moves_11.append(get_x_value(state - 2052))
-    else:
-        possible_moves_11.append(0)
-
-    if drop_state[1] == '1' and drop_state[8] == '1': # 2 and 9 available
-        possible_moves_11.append(get_x_value(state - 1032))
-    else:
-        possible_moves_11.append(0)
-
-    if drop_state[2] == '1' and drop_state[7] == '1': # 3 and 8 available
-        possible_moves_11.append(get_x_value(state - 528))
-    else:
-        possible_moves_11.append(0)
-
-    if drop_state[3] == '1' and drop_state[6] == '1': # 4 and 7 available
-        possible_moves_11.append(get_x_value(state - 288))
-    else:
-        possible_moves_11.append(0)
-        
-    if drop_state[4] == '1' and drop_state[5] == '1': # 5 and 6 available
-        possible_moves_11.append(get_x_value(state - 192))
-    else:
-        possible_moves_11.append(0)
-
-    if drop_state[10] == '1': # 11 available
-        possible_moves_11.append(get_x_value(state - 2))
-    else:
-        possible_moves_11.append(0)
-
-    x_values.append(np.max(possible_moves_11))
-    ind11 = possible_moves_11.index(np.max(possible_moves_11))
-    od_11.append(move_names_11[ind11])
-
-    # if you roll 12
-    move_names_12 = ['0', '1 11', '2 10', '3 9', '4 8', '5 7', '12']
-    possible_moves_12 = [0]
-    if drop_state[0] == '1' and drop_state[10] == '1': # 1 and 11 available
-        possible_moves_12.append(get_x_value(state - 2050))
-    else:
-        possible_moves_12.append(0)
-
-    if drop_state[1] == '1' and drop_state[9] == '1': # 2 and 10 available
-        possible_moves_12.append(get_x_value(state - 1028))
-    else:
-        possible_moves_12.append(0)
-
-    if drop_state[2] == '1' and drop_state[8] == '1': # 3 and 9 available
-        possible_moves_12.append(get_x_value(state - 520))
-    else:
-        possible_moves_12.append(0)
-
-    if drop_state[3] == '1' and drop_state[7] == '1': # 4 and 8 available
-        possible_moves_12.append(get_x_value(state - 272))
-    else:
-        possible_moves_12.append(0)
-
-    if drop_state[4] == '1' and drop_state[6] == '1': # 5 and 7 available
-        possible_moves_12.append(get_x_value(state - 160))
-    else:
-        possible_moves_12.append(0)
-
-    if drop_state[11] == '1': # 12 available
-        possible_moves_12.append(get_x_value(state - 1))
-    else:
-        possible_moves_12.append(0)
-
-    x_values.append(np.max(possible_moves_12))
-    ind12 = possible_moves_12.index(np.max(possible_moves_12))
-    od_12.append(move_names_12[ind12])
-    
-    return x_values
-
 def calc_x_value(drop_state):
     x_values = get_x_values(drop_state)
     sum = 0
-    for i in range(2, 13):
+    for i in range(2, 13): # calculate expected value of a state with weighted sum of probabilities and expected values after each roll
         sum += (prob_dict[i] * x_values[i])
     return sum
 
 def get_x_value(drop_state): # drop_state: an int that represents the current state of the lemondrops when converted to binary
     return x_value_dict.get(drop_state)
 
-for i in range(1, 4096):
+def get_x_values(state): # returns a list of expected values for each roll. Index 2 = x_value if a 2 is rolled with the current state, etc.
+    drop_state = int_to_bin(state)
+    x_values = [0, 0] # not possible to roll 0 or 1, so set those values to 0
+    
+    for roll in range(2, 13):
+        valid_moves = get_valid_moves(roll)
+        possible_moves = [] # expected values of all possible moves for roll
+        for move in valid_moves:
+            if move == [0]: # expected value for a lost position = 0
+                possible_moves.append(0)
+                continue
+            move_valid = True 
+            for i in move: # check validity by checking if any number needed is unavailable (corresponding bit in drop_state = 0)
+                if drop_state[i - 1] == '0': # move is invalid
+                    possible_moves.append(0)
+                    move_valid = False
+                    break
+            if move_valid:
+                next_state = state
+                for i in move: # determine the next state if you play this move: reset corresponding bits of state
+                    next_state = next_state - 2**(12 - i) 
+                possible_moves.append(get_x_value(next_state)) # get expected value by checking expected value of next state
+        x_values.append(max(possible_moves)) # take max to get the expected value of the optimal move for the roll
+        optimal_decisions[roll].append(valid_moves[possible_moves.index(max(possible_moves))])
+
+    return x_values
+
+# iterate through all possible game states, starting from next simplest after the win state
+for i in range(1, 4096): 
     x_value_dict[i] = calc_x_value(i)
 
-# place results into a list and write to a text file
-file = open('dropsolver.txt', 'w')
-x_val_list = [1]
+# make a table and write results into a text file
+file = open(target_name, 'w')
 
-for i in range(1, 4096):
-    x_val_list.append(calc_x_value(i))
+def format_move(move):
+    move_str = ''
+    for i in move:
+        move_str += str(i) + ' '
+    return move_str
 
-# format table
 t = PrettyTable(['Drop State', 'Expected Value', 'Dice Sum = 2', 'Dice Sum = 3', 'Dice Sum = 4', 'Dice Sum = 5', 
                  'Dice Sum = 6', 'Dice Sum = 7', 'Dice Sum = 8', 'Dice Sum = 9', 'Dice Sum = 10', 'Dice Sum = 11', 
                  'Dice Sum = 12'])
 for j in range(0, 4096):
-    i = 4095 - j
-    t.add_row([int_to_bin(i), x_val_list[i], od_2[i], od_3[i], od_4[i], od_5[i], od_6[i], od_7[i], od_8[i], 
-               od_9[i], od_10[i], od_11[i], od_12[i]])
+    i = 4095 - j # go through game states from most complex to simplest
+    t.add_row([int_to_bin(i), x_value_dict[i]] + [format_move(optimal_decisions[k][i]) for k in range(2, 13)])
 
 file.write(str(t))
 file.close()
